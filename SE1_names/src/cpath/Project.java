d3390ed7699147ce186fd2a56fb8e4b2e3e70b68
huettnto <huettner.timo@gmail.com>
2018-04-27 02:37:53 +0200

    Add files via upload

--- SE1_names/src/cpath/Project.java
+++ SE1_names/src/cpath/Project.java
@@ -1,5 +1,6 @@
 package cpath;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -7,12 +8,9 @@ import java.util.Set;
 import java.util.Stack;
 
 /**
- * @author Jena Satkunarajan, 2965839, st116472@stud.uni-stuttgart.de
-* @author Jonas Allali, 2965826, st116462@stud.uni-stuttgart.de
- * @author Heinrich Pauli, 324875
- * @author Timo Hüttner, 3220544, st148236@stud.uni-stuttgart.de
- * 
- * 
+ * Stellt ein Projekt dar, das aus Arbeitspaketen besteht. Im Prinzip nichts
+ * anderes als ein Graph mit Arbeitspaketen als Knoten. Das Projekt kennt nur
+ * die Startknoten, diese kennen jeweils ihren Nachfolger.
  */
 public class Project {
 
@@ -21,9 +19,6 @@ public class Project {
 	private Set<Workpackage> criticalPathNodes = new HashSet<Workpackage>();
 	private Set<Workpackage> allNodes = new HashSet<Workpackage>();
 	private Stack<Workpackage> currentNodes = new Stack<Workpackage>();
-
-	//the latest finish of any workpackage of the project (i.e. the project duration) 
-	private int maxEF = 0;
 
 	public List<Workpackage> getStartNodes() {
 		return startNodes;
@@ -41,124 +36,95 @@ public class Project {
 		return allNodes;
 	}
 
-	
-	// TODO: Implementieren Sie diese Methode, so dass nach deren Aufruf
-	// alle Werte im Graph gesetzt sind und die beiden verfuegbaren Sets
-	// korrekt befuellt wurden. Veraendern Sie nicht die Workpackage-Klasse!
-	// Zum Testen koennen Sie die beigefuegte Test-Klasse verwenden und
-	// erweitern. Beachten Sie, dass ihre Implementierung generell
-	// funktionieren und nicht nur dieses eine Problem loesen soll.
 	public void computeCriticalPath() {
 		resetLists();
+		// TODO: Implementieren Sie diese Methode, so dass nach deren Aufruf
+		// alle Werte im Graph gesetzt sind und die beiden verfuegbaren Sets
+		// korrekt befuellt wurden. Veraendern Sie nicht die Workpackage-Klasse!
+		// Zum Testen koennen Sie die beigefuegte Test-Klasse verwenden und
+		// erweitern. Beachten Sie, dass ihre Implementierung generell
+		// funktionieren und nicht nur dieses eine Problem loesen soll.
 
 		/*
-		 * process starting nodes: set ES and EF, check for maxEF and push all
-		 * successor nodes on the processing stack
-		 * 
+		 * start nodes
 		 */
-		for (Workpackage startNode : this.startNodes) {
-			allNodes.add(startNode);
-			startNode.setEarliestStart(0);
-			startNode.setEarliestFinish(startNode.getDuration());
-			if (startNode.getEarliestFinish() > maxEF)
-				maxEF = startNode.getEarliestFinish();
-			if (!startNode.getSuccessors().isEmpty()) {
-				this.currentNodes.addAll(startNode.getSuccessors());
-			} else {
-				endNodes.add(startNode);
-			}
+		List<Workpackage> startNodes = getStartNodes();
+		for (Workpackage startnode : startNodes) {
+			startnode.setEarliestStart(0);
+			currentNodes.push(startnode);
 		}
 
-		/*
-		 * depth-first processing of the successor nodes: set ES (for which the
-		 * maxEF of all pred. is needed), EF, check for maxEF, push successors
-		 * on stack OR (of no succ.) put it into the endnodes-list
-		 */
-		while (!this.currentNodes.isEmpty()) {
-			Workpackage currNode = currentNodes.pop();
-			allNodes.add(currNode);
-			currNode.setEarliestStart(this.getMaxEFOfPred(currNode));
-			currNode.setEarliestFinish(currNode.getEarliestStart() + currNode.getDuration());
-			if (currNode.getEarliestFinish() > maxEF)
-				maxEF = currNode.getEarliestFinish();
-			if (!currNode.getSuccessors().isEmpty()) {
-				currentNodes.addAll(currNode.getSuccessors());
+		// earliest start and finish
+		while (!currentNodes.isEmpty()) {
+			Workpackage currentNode = currentNodes.pop();
+			List<Workpackage> currentSuccessors = currentNode.getSuccessors();
+			List<Workpackage> currentPredecessors = currentNode.getPredecessors();
+			currentNodes.addAll(currentSuccessors);
+
+			if (!currentPredecessors.isEmpty()) {
+				int x = Integer.MIN_VALUE;
+				for (Workpackage preNode : currentPredecessors) {
+					if (x < preNode.getEarliestStart() + preNode.getDuration()) {
+						x = preNode.getEarliestStart() + preNode.getDuration();
+					}
+				}
+				currentNode.setEarliestFinish(x + currentNode.getDuration());
+				currentNode.setEarliestStart(x);
 			} else {
-				this.endNodes.add(currNode);
+				currentNode.setEarliestFinish(currentNode.getDuration());
+			}
+
+			// adds the current node to the endNodes list if it is an end node.
+			if (currentSuccessors.isEmpty()) {
+				if (!endNodes.contains(currentNode)) {
+					endNodes.add(currentNode);
+				}
+			}
+			// adds the current node to the node set if it is not in there yet
+			allNodes.add(currentNode);
+		}
+		// calc the latest finish
+		int LF = Integer.MIN_VALUE;
+		for (Workpackage endNode : endNodes) {
+			if (LF < endNode.getEarliestFinish()) {
+				LF = endNode.getEarliestFinish();
 			}
 		}
-
-		/*
-		 * after processing all the nodes in the graph, the maxEF is known: set
-		 * LS (= maxEF), LF and slack of all endnodes; if slack is zero, add the
-		 * node to the critical path. Add all predecessors to the processing
-		 * stack
-		 * 
-		 */
-		for (Workpackage endNode : this.endNodes) {
-			endNode.setLatestFinish(maxEF);
-			endNode.setLatestStart(endNode.getLatestFinish() - endNode.getDuration());
-			endNode.setSlack(endNode.getLatestStart() - endNode.getEarliestStart());
-			if (endNode.getSlack() == 0)
-				this.criticalPathNodes.add(endNode);
-			this.currentNodes.addAll(endNode.getPredecessors());
+		// sets the latest finish, latest start and slack for the end nodes
+		for (Workpackage endNode : endNodes) {
+			endNode.setLatestFinish(LF);
+			endNode.setLatestStart(LF - endNode.getDuration());
+			endNode.setSlack(endNode.getLatestFinish() - endNode.getEarliestFinish());
 		}
 		
-		/*
-		 * "reverse" depth first processing of predecessors: basically the same
-		 * thing as for the endnodes
-		 */
-		while (!this.currentNodes.isEmpty()) {
-			Workpackage currNode = currentNodes.pop();
-			currNode.setLatestFinish(this.getMinLSOfSucc(currNode));
-			currNode.setLatestStart(currNode.getLatestFinish() - currNode.getDuration());
-			currNode.setSlack(currNode.getLatestStart() - currNode.getEarliestStart());
-			if (currNode.getSlack() == 0)
-				this.criticalPathNodes.add(currNode);
-			currentNodes.addAll(currNode.getPredecessors());
+		//starting at the end nodes
+		currentNodes.addAll(endNodes);
+		while (!currentNodes.isEmpty()) {
+			Workpackage currentNode = currentNodes.pop();
+			List<Workpackage> currentSuccessors = currentNode.getSuccessors();
+			List<Workpackage> currentPredecessors = currentNode.getPredecessors();
+			currentNodes.addAll(currentPredecessors);
+
+			if (!currentSuccessors.isEmpty()) {
+				int x = Integer.MAX_VALUE;
+				for (Workpackage succNode : currentSuccessors) {
+					if (x > succNode.getLatestStart() - currentNode.getEarliestFinish()) {
+						x = succNode.getLatestStart() - currentNode.getEarliestFinish();
+					}
+				}
+					currentNode.setSlack(x);
+					currentNode.setLatestFinish(currentNode.getEarliestFinish() + currentNode.getSlack());
+					currentNode.setLatestStart(currentNode.getEarliestStart() + currentNode.getSlack());
+			}
 		}
-
-	}
-
-	/**
-	 * compares the EF's of all the predecessors of the given node and determines the maxEF
-	 * 
-	 * 
-	 * @param node  the node for which the maximum EF of all its predecessors has to be determined
-	 * @return      the maxEF of the nodes predecessors
-	 */
-	private int getMaxEFOfPred(Workpackage node) {
-		int maxEF = 0;
-		for (Workpackage pred : node.getPredecessors()) {
-			if (pred.getEarliestFinish() > maxEF)
-				maxEF = pred.getEarliestFinish();
+		
+		//adds the critical path nodes to the set
+		for (Workpackage node : allNodes) {
+			if (node.getSlack()==0) {
+				criticalPathNodes.add(node);
+			}
 		}
-		return maxEF;
 	}
-
-	/**
-	 * compares the LS' of all the successors of the given node and determines the minLS
-	 * 
-	 * 
-	 * @param node  the node for which the minimum LS of all its successors has to be determined
-	 * @return      the minLS of the nodes successors
-	 */
-	private int getMinLSOfSucc(Workpackage node){
-		int minLS = Integer.MAX_VALUE;
-		for(Workpackage succ : node.getSuccessors()){
-			if(succ.getLatestStart() < minLS) minLS = succ.getLatestStart();
-		}
-		return minLS;
-	}
-	
-	/**
-	 * 
-	 * @return the critical path nodes
-	 */
-	private Set<Workpackage> getCriticalPath(){
-		return this.criticalPathNodes;
-	}
-
 
 	private void resetLists() {
 		currentNodes.clear();
